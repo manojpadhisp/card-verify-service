95c3a68a4804674eb9b01521cd4dbef40b1c4e0a
Manoj <manojpadhisp@gmail.com>
2023-08-17 07:24:12 +0530

    1st

---
+++ src/test/java/com/tesco/cardverify/CardVerifyServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tesco.cardverify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardVerifyServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
